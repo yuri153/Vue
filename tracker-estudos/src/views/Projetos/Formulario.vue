b80665bbe34313066ff5e6e1d0bdb50313b967bb
Yuri Vinicius <yuriprodigio@hotmail.com>
2025-04-25 16:55:15 -0300

    Adicionar sistema de notificação e componentes relacionados
    - Introduzido o componente `Notificacoes` em `App.vue` para exibir notificações.
    - Ajustado o layout em `Tarefa.vue` para incluir nomes de projetos.
    - Atualizado o repositório Vuex em `index.ts` para gerenciar notificações com uma nova propriedade de estado e mutação.
    - Criado `INotificacao.ts` para definir a estrutura e os tipos de notificação.
    - Aprimorado o `Formulario.vue` para disparar notificações ao adicionar um projeto.
    - Adicionado `notificador.ts` para encapsular a lógica de notificação e um mixin para facilitar o uso entre componentes.

--- tracker-estudos/src/views/Projetos/Formulario.vue
+++ tracker-estudos/src/views/Projetos/Formulario.vue
@@ -22,6 +22,9 @@ import { useStore } from "@/store";
 import { defineComponent } from "vue";
 
 import { ALTERA_PROJETO, ADICIONA_PROJETO } from '@/store/tipo-mutacoes'
+import { TipoNotificacao } from "@/interfaces/INotificacao";
+
+import useNotificador from '@/hooks/notificador'
 
 export default defineComponent({
   name: "Formulario",
@@ -51,14 +54,18 @@ export default defineComponent({
       } else {
         this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
       }
+
       this.nomeDoProjeto = "";
+      this.notificar(TipoNotificacao.SUCESSO, 'Excelente!', 'O projeto foi cadastrado com sucesso!')
       this.$router.push('/projetos')
-    },
+    }
   },
   setup () {
     const store = useStore()
+    const { notificar } = useNotificador()
     return {
-      store
+      store,
+      notificar
     }
   }
 });
