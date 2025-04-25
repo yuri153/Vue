216b90b3f4861f8de64c1f2aa86455fdc40550f0
Yuri Vinicius <yuriprodigio@hotmail.com>
2025-04-25 15:47:54 -0300

    Refatoração de componentes e roteamento de gerenciamento de projetos
    - Remoção do componente `HelloWorld.vue`.
    - Introdução de roteamento aninhado para `Projetos` com rotas filhas para listagem e edição de projetos.
    - Atualização do repositório Vuex para usar constantes para tipos de mutação.
    - Modificação de `Projetos.vue` para usar `<router-view>` e remoção do formulário de projeto em linha.
    - Criação de `Formulario.vue` para criação e edição de projetos.
    - Adição de `Lista.vue` para exibir projetos com opções de edição e exclusão.
    - Adição de `tipo-mutacoes.ts` para constantes de tipo de mutação.

--- tracker-estudos/src/views/Projetos.vue
+++ tracker-estudos/src/views/Projetos.vue
@@ -1,62 +1,18 @@
 <template>
-  <section class="projetos">
+  <div class="projetos">
     <h1 class="title">Projetos</h1>
-    <form @submit.prevent="salvar">
-      <div class="field">
-        <label for="nomeDoProjeto" class="label"> Nome do Projeto </label>
-        <input
-          type="text"
-          class="input"
-          v-model="nomeDoProjeto"
-          id="nomeDoProjet"
-        />
-      </div>
-      <div class="field">
-        <button class="button" type="submit">Salvar</button>
-      </div>
-    </form>
-    <table class="table is-fullwidth">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nome</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="projeto in projetos" :key="projeto.id">
-          <td>{{ projeto.id }}</td>
-          <td>{{ projeto.nome }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </section>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script lang="ts">
-import { useStore } from "@/store";
-import { computed, defineComponent } from "vue";
+
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: "Projetos",
-  data() {
-    return {
-      nomeDoProjeto: ""
-    };
-  },
-  methods: {
-    salvar() {
-      this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
-      this.nomeDoProjeto = "";
-    },
-  },
-  setup () {
-    const store = useStore()
-    return {
-      store,
-      projetos: computed(() => store.state.projetos)
-    }
-  }
-});
+  name: 'Projetos'
+})
+
 </script>
 
 <style scoped>
